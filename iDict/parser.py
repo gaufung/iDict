001219d23801561040d124dc2c3ee09d59512f5a
gaufung <gaufung@outlook.com>
2017-10-08 13:51:35 +0800

    use sqlalchemy

--- iDict/parser.py
+++ iDict/parser.py
@@ -3,8 +3,7 @@ parse a word to word object
 """
 import os
 from bs4 import BeautifulSoup
-from iDict.word import Word
-from iDict.util import query_word
+from iDict.word import Word, Explain, Sentence
 
 
 class ParserError(Exception):
@@ -20,50 +19,44 @@ class Parser(object):
 
 
 class DbParser(Parser):
-    def __init__(self, successor=None):
-        self.successor = successor
+    def __init__(self, session):
+        self.session = session
 
     def parse(self, text):
-        try:
-            word = query_word(text)
-            if not word:
-                raise ParserError('Cannot look up from database')
-            return word
-        except ParserError:
-            return self.successor.parse(text)
-        except Exception as err:
-            raise err
+        word = self.session.query(Word).filter(Word.name == text).first()
+        if not word:
+            raise ParserError('Cannot look up from database')
+        return word
 
 
 class BingParser(Parser):
 
     url = 'http://cn.bing.com/dict/search?q={%s}'
 
-    def __init__(self, successor=None):
-        self.successor = successor
+    def __init__(self, session):
+        self.session = session
 
     def _parse(self, text):
         soup = BeautifulSoup(open('search?q=%s' % text, encoding='utf-8'), 'lxml')
         definition_tags = soup.find_all(class_='def')
-        chinese = []
+        word = Word(name=text)
+        self.session.add(word)
         for tag in definition_tags:
-            chinese.append(tag.string)
+            self.session.add(Explain(content=tag.string, word=word))
         sentence_tags = soup.find_all(class_='sen_en')
-        sentences = []
         for tag in sentence_tags:
             words = []
             for child in tag.children:
                 words.append(child.string)
-            sentences.append(''.join(words))
-        return Word(text, os.linesep.join(chinese), os.linesep.join(sentences))
+            self.session.add(Sentence(content=''.join(words), word=word))
+        self.session.commit()
 
     def parse(self, text):
         try:
             query_url = self.url % text
             os.system('curl -O %s' % query_url)
-            word = self._parse(text)
+            self._parse(text)
             os.remove('search?q=%s' % text)
-            return word
         except ParserError:
             if self.successor is None:
                 raise Exception('Having no handler')
@@ -71,10 +64,4 @@ class BingParser(Parser):
                 self.successor.parse(text)
         except Exception as err:
             raise err
-
-
-def lookup(handler):
-    def _parse(text):
-        return handler.parse(text)
-    return _parse
 
--- iDict/word.py
+++ iDict/word.py
@@ -1,50 +1,42 @@
 """
 The class of word using SQLAlchemy orm framework
 """
-import os
+from sqlalchemy import Column, DateTime, Text, Integer, func, String, ForeignKey
+from sqlalchemy.orm import relationship, backref
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
 
 
-class Word(object):
-    def __init__(self, name, chinese, sentences, priority=1):
-        self._name = name
-        self._chinese = chinese
-        self._sentences = sentences
-        self._property = priority
-
-    @property
-    def name(self):
-        return self._name
-
-    @property
-    def chinese(self):
-        return self._chinese
-
-    @property
-    def sentences(self):
-        return self._sentences
-
-    @property
-    def priority(self):
-        return self._property
-
-    def __str__(self):
-        return self.name
-
-    def display(self):
-        shows = list()
-        shows.append(os.linesep)
-        shows.append(self.name)
-        shows.append(os.linesep)
-        shows.append('******Chinese definitions******')
-        shows.append(os.linesep)
-        shows.append(self.chinese)
-        shows.append(os.linesep)
-        shows.append('******Sentences********')
-        shows.append(os.linesep)
-        shows.append(self.sentences)
-        shows.append(os.linesep)
-        return ''.join(shows)
+class Word(Base):
+    __tablename__ = 'word'
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False)
+    lookup_on = Column(DateTime, default=func.now())
+    priority = Column(Integer, default=1)
 
 
+class Explain(Base):
+    __tablename__ = 'explain'
+    id = Column(Integer, primary_key=True)
+    content = Column(Text)
+    word_id = Column(Integer, ForeignKey('word.id'))
+    word = relationship(
+        Word,
+        backref=backref('explains',
+                        uselist=True,
+                        cascade='delete,all')
+    )
 
 
+class Sentence(Base):
+    __tablename__ = 'sentence'
+    id = Column(Integer, primary_key=True)
+    content = Column(Text)
+    word_id = Column(Integer, ForeignKey('word.id'))
+    word = relationship(
+        Word,
+        backref=backref('sentences',
+                        uselist=True,
+                        cascade='delete,all')
+    )
